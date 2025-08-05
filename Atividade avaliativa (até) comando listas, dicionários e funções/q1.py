74d336218e3e14614d2248e0657558576621fea5
riquelme <164274153+theRhen@users.noreply.github.com>
2025-08-05 17:39:32 -0300

    Update q1.py

--- Atividade avaliativa (até) comando listas, dicionários e funções/q1.py
+++ Atividade avaliativa (até) comando listas, dicionários e funções/q1.py
@@ -1,1 +1,29 @@
+def cpf_valido (cpf : str):
+  if type(cpf) == str:
+    cpf = cpf.replace(".","").replate("-","")
+    
+  if cpf.isdecimal() == False:
+  cpf.replace(".","").replate("-","")
+  
+soma = 0
+for pos in range (9):
+    soma += int(cpf[pos]) * (10 - pos)
+dv = 11 - soma % 11
+if dv1 >= 10: 
+    dv1 = 0
 
+
+if dv1 != int(cpf[9]):
+    return False
+
+soma = 0
+for pos in range (10):
+    soma += int(cpf[pos]) * (11 - pos)
+dv = 11 - soma % 11
+if dv2 >= 10: 
+    dv2 = 0
+
+if dv2 != int(cpf[10]):
+    return False
+
+cpf = input("Digite seu CPF")
