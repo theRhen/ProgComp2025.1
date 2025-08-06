2d9b2d6fed0f2e0a5abd6ad4143069715cf4694a
riquelme <164274153+theRhen@users.noreply.github.com>
2025-08-06 02:00:27 -0300

    Add files via upload

---
+++ Atividade avaliativa (até) comando listas, dicionários e funções/termoo.py
@@ -0,0 +1,49 @@
+import random
+palavras = (
+    "ADAGA", "ADUBO", "AMIGO", "ANEXO", "ARAME", "ARARA", "ARROZ",
+    "ASILO", "ASTRO", "BAILE", "BAIXA", "BALAO", "BALSA", "BARCO",
+    "BARRO", "BEIJO", "BICHO", "BORDA", "BORRA", "BRAVO", "BREJO",
+    "BURRO", "CAIXA", "CALDO", "CANJA", "CARRO", "CARTA", "CERVO",
+    "CESTA", "CLIMA", "COBRA", "COLAR", "COQUE", "COURO", "CRAVO",
+    "DARDO", "FAIXA", "FARDO", "FENDA", "FERRO", "FESTA", "FLUOR",
+    "FORCA", "FORNO", "FORTE", "FUNDO", "GAITA", "GARRA", "GENIO",
+    "GESSO", "GRADE", "GRANA", "GRAMA", "GURIA", "GREVE", "GRUTA",
+    "HEROI", "HOTEL", "ICONE", "IMPAR", "IMUNE", "INDIO", "JUNTA",
+    "LAPIS", "LARVA", "LAZER", "LENTO", "LESTE", "LIMPO", "LIVRO",
+    "MACIO", "MAGRO", "MALHA", "MANSO", "MARCO", "METAL", "MORTE",
+    "MORRO", "MURAL", "MOVEL", "NACAO", "NINHO", "NOBRE", "NORMA",
+    "NORTE", "NUVEM", "PACTO", "PALHA", "PARDO", "PARTE", "PEDRA",
+    "PEDAL", "PEIXE", "PRADO", "PISTA", "POMBO", "POETA", "PONTO",
+    "PRATO", "PRECO", "PRESO", "PROSA", "PRUMO", "PULGA", "PULSO",
+    "QUEPE", "RAIVA", "RISCO", "RITMO", "ROSTO", "ROUPA", "SABAO",
+    "SALTO", "SENSO", "SINAL", "SITIO", "SONHO", "SOPRO", "SURDO",
+    "TARDE", "TERNO", "TERMO", "TERRA", "TIGRE", "TINTA", "TOLDO",
+    "TORRE", "TRAJE", "TREVO", "TROCO", "TRONO", "TURMA", "URUBU",
+    "VALSA", "VENTO", "VERDE", "VISAO", "VINHO", "VIUVO", "ZEBRA"
+)
+
+'''
+ Seis tentativas
+  1 - Impossível
+  2 - Ninja
+  3 - Impressionante
+  4 - Interessante
+  5 - Pode melhorar
+  6 - Foi por pouco
+'''
+
+'''
+ Cores/estilos de texto em terminal:
+    \033[N;N;Nm'
+
+        1  a  6 - estilo: negrito, piscante, itálico
+        31 a 37 - cor de texto
+        41 a 47 - cor de background
+
+    \033[m' - reset da cor/estilo
+
+    Detalhes em:
+        https://www.dio.me/articles/colocando-cores-no-seu-programa-em-python
+'''
+
+print (random.choice(palavras))
